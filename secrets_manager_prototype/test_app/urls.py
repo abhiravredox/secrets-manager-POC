e8b2d2d4b8f61c225cde606fc145b07bc8bbe9cb
Abhirav Kariya <akariya@ncsu.edu>
2020-03-18 20:11:31 -0400

    UI done, need to add how-to-use

--- secrets_manager_prototype/test_app/urls.py
+++ secrets_manager_prototype/test_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.registred, name='registered'),
     path('<str:env_name>', views.registred, name='registered_show_detail'),
-    path('reload/<str:env_name>/', views.reload_registered, name='reload_registered')
+    path('reload/<str:env_name>/', views.reload_registered, name='reload_registered'),
+    path('deployed/', views.deployed, name='deployed'),
+    path('reload_deployed/', views.reload_deployed, name='reload_deployed'),
 ]
--- secrets_manager_prototype/test_app/views.py
+++ secrets_manager_prototype/test_app/views.py
@@ -7,19 +7,18 @@ secrets_manager = SecretsManager()
 
 
 def registred(request, env_name=None):
-
     if env_name is None:
         env_name = secrets_manager.deploy_env
     template = loader.get_template('test_app/registered.html')
     secrets = secrets_manager.get_secrets(env_name)
     base = secrets_manager.base
     secrets_object = secrets_manager.env_secrets[env_name]
-    secrets_types = ["Cannot determine until loaded" if env not in secrets_manager.env_secrets else type(secrets_manager.env_secrets[env]).__name__
+    secrets_types = ["Cannot determine until loaded" if env not in secrets_manager.env_secrets else type(
+        secrets_manager.env_secrets[env]).__name__
                      for env in secrets_manager.env_configs]
     context = {
         'secrets': secrets,
         'env_name': env_name,
-        'heading': 'Registered Environment Settings',
         'display_env': secrets_manager.env_secrets[env_name],
         'base_env_name': base,
         'base_env': secrets_manager.env_secrets[base],
@@ -42,6 +41,35 @@ def registred(request, env_name=None):
 
 
 def reload_registered(request, env_name):
-    print("herr")
     secrets_manager.env_secrets[env_name].reload()
     return registred(request, env_name)
+
+
+def deployed(request):
+    env_name = secrets_manager.deploy_env
+    template = loader.get_template('test_app/deployed.html')
+    secrets = secrets_manager.get_secrets(env_name)
+    base = secrets_manager.base
+    secrets_object = secrets_manager.env_secrets[env_name]
+    context = {
+        'secrets': secrets,
+        'env_name': env_name,
+        'display_env': secrets_manager.env_secrets[env_name],
+        'base_env_name': base,
+        'base_env': secrets_manager.env_secrets[base],
+        'deployed_page': 'active',
+        'display_details': True,
+        'env_config': {'Source': secrets_object.src,
+                       'Payload': secrets_object.payload,
+                       'Headers': secrets_object.headers,
+                       'Auto Reload': secrets_object.auto_reload,
+                       },
+        'env_secrets_type': type(secrets_object).__name__
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def reload_deployed(request):
+    secrets_manager.env_secrets[secrets_manager.deploy_env].reload()
+    return deployed(request)
